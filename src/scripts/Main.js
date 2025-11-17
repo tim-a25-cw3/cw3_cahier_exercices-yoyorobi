cc7cdf1975c8282aac26fbf43e08adbaf1452b2e
Yrobitaille <yoan.robitaille@hotmail.com>
2025-11-17 12:37:10 -0500

    Cour fini

--- src/scripts/Main.js
+++ src/scripts/Main.js
@@ -1,12 +1,23 @@
 import Icons from './utils/Icons.js';
+import Swiper from 'swiper/bundle';
 
 class Main {
   constructor() {
     this.init();
   }
-
   init() {
     Icons.load();
+    this.initSwiper();
+  }
+  initSwiper() {
+    const target = document.querySelector('.js-swiper');
+    if (target) {
+      const swiper = new Swiper(target, {
+        pagination: {
+          el: '.swiper-pagination',
+        },
+      });
+    }
   }
 }
 new Main();
